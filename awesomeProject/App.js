724f98bda85fe85b227c141f6bdcadba5195bf82
Léo Liu <leoliuhy@gmail.com>
2022-05-21 12:42:47 -0400

    first UI commit

--- awesomeProject/App.js
+++ awesomeProject/App.js
@@ -1,11 +1,12 @@
 // Speech to Text Conversion in React Native – Voice Recognition
 // https://aboutreact.com/speech-to-text-conversion-in-react-native-voice-recognition/
- 
+
 // import React in our code
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Audio, Video } from 'expo-av';
- 
+
+
 // import all the components we are going to use
 import {
   SafeAreaView,
@@ -13,66 +14,127 @@ import {
   Text,
   View,
   Image,
+  Dimensions,
   TouchableHighlight,
   ScrollView,
   Button,
 } from 'react-native';
- 
+
 // import Voice
 import Voice from 'react-native-voice';
- 
-const App = () => {
+
+
+function App() {
   const [recording, setRecording] = useState();
- 
-  useEffect(async() => {
+  const [isRecording, setIsRecording] = useState();
+
+  useEffect(async () => {
     //Setting callbacks for the process status
     const permission = await Audio.requestPermissionsAsync();
 
     await Audio.setAudioModeAsync({
       allowsRecordingIOS: true,
       playsInSilentModeIOS: true,
-    }); 
- 
+    });
+
     return () => {
       //destroy the process after switching the screen
       Voice.destroy().then(Voice.removeAllListeners);
     };
   }, []);
 
-  async function startRecording(){
+  async function manageRecording() {
+    if (isRecording) {
+      setIsRecording(false);
+      const player = new Audio.Sound();
+
+      setRecording(undefined);
+      await recording.stopAndUnloadAsync();
+      const uri = recording.getURI();
+      console.log('Recording stopped and stored at', uri);
+
+      await player.loadAsync({ uri: uri });
+      await player.playAsync();
+    } else {
+      setIsRecording(true);
       console.log('Starting recording..');
       const { recording } = await Audio.Recording.createAsync(
         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
       );
       setRecording(recording);
+    }
   }
 
-  async function stopRecording(){
+  async function startRecording() {
+    if (isRecording) return;
+    isRecording = true;
+    console.log('Starting recording..');
+    const { recording } = await Audio.Recording.createAsync(
+      Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+    );
+    setRecording(recording);
+  }
+
+  async function stopRecording() {
+    if (!isRecording) return;
+    isRecording = false;
     const player = new Audio.Sound();
 
     setRecording(undefined);
     await recording.stopAndUnloadAsync();
-    const uri = recording.getURI(); 
+    const uri = recording.getURI();
     console.log('Recording stopped and stored at', uri);
 
-    await player.loadAsync({uri: uri});
+    await player.loadAsync({ uri: uri });
     await player.playAsync();
   }
- 
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Text>Hi!</Text>
-        <Button title="Start" onPress={startRecording} style={styles.buttonTextStyle}></Button>
-        <Button title="Stop" onPress={stopRecording} style={styles.buttonTextStyle}></Button>
+        <Text style={styles.bigTitle} top={100}><i>Speech<br></br>Note</i></Text>
+        {/* <Button title="Start" onPress={startRecording} style={styles.microphoneButton}></Button>
+        <Button title="Stop" onPress={stopRecording} color="#bbbbbb"></Button> */}
+
+        <TouchableHighlight
+          style={{
+            borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+            width: Dimensions.get('window').width * 0.65,
+            height: Dimensions.get('window').width * 0.65,
+            backgroundColor: '#879CD3',
+            justifyContent: 'center',
+            alignItems: 'center',
+            top: Dimensions.get('window').height * 0.3
+          }}
+          underlayColor='#ccc'
+          onPress={() => manageRecording()}
+        >
+          {/* <Text>sdfsdf</Text> */}
+          <Image
+            style={styles.microphoneIcon}
+            source={
+              require('./microphone.png')
+            }
+          />
+        </TouchableHighlight>
       </View>
     </SafeAreaView>
   );
 };
- 
+
 export default App;
- 
+
 const styles = StyleSheet.create({
+  bigTitle: {
+    fontSize: 50,
+    fontFamily: "Inter",
+    textAlign: "center",
+  }
+  ,
+  microphoneIcon: {
+    width: 70,
+    height: 70,
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -94,14 +156,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 15,
     padding: 10,
-    backgroundColor: '#8ad24e',
+    backgroundColor: '#eb4034',
     marginRight: 2,
     marginLeft: 2,
   },
   buttonTextStyle: {
-    color: '#fff',
+    color: '#cccccc',
     textAlign: 'center',
     width: 50
+  },
+  microphoneButton: {
+    color: '#bbbbbb',
+    textAlign: 'left',
+    width: 100
   },
   horizontalView: {
     flexDirection: 'row',
