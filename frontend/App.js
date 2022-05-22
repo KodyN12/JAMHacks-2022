9792c867798a853e407c66c4df3ab4587574f0e2
Adam <adamomarali37@gmail.com>
2022-05-21 20:30:04 -0400

    Able to post data

--- frontend/App.js
+++ frontend/App.js
@@ -32,6 +32,13 @@ const App = () => {
       allowsRecordingIOS: true,
       playsInSilentModeIOS: true,
     }); 
+    
+
+    fetch("http://localhost:3000/")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    // await fetch("http://localhost:3000/", options).then(data => console.log(data))
+    
  
     return () => {};
   }, []);
@@ -48,6 +55,13 @@ const App = () => {
 
       await player.loadAsync({ uri: uri });
       await player.playAsync();
+
+      fetch("http://localhost:3000/", {
+        method: 'POST',
+        headers:{ 'Content-Type':'application/json' },
+        body: JSON.stringify({uri : uri})
+      })
+      .then(res => res.text())
       
     } else {
       setIsRecording(true);
